794050ae5f7379352a05df4fbcae481acba0edce
vikasindve <vikasindve04@gmail.com>
2020-09-05 14:15:51 +0530

    second time

---
+++ BasicCoreJava/src/variablesAndDataTypes/DataTypes.java
@@ -0,0 +1,14 @@
+package variablesAndDataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age=32;
+		String name="vikas";
+		char gender='M';
+		boolean MarritailStatus=true;
+	}
+
+}
---
+++ BasicCoreJava/src/variablesAndDataTypes/FirtsJavaClass.java
@@ -0,0 +1,9 @@
+package variablesAndDataTypes;
+
+public class FirtsJavaClass {
+	 public static void main(String[] args) {
+		 System.out.println("First Java Class");
+		 
+	 }
+
+}
